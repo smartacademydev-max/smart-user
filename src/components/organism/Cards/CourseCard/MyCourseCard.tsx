3817de415a707151bcf3ce1c9e26e07216c450ef
arjun <arjunjhukal.makura@gmail.com>
2026-02-06 17:14:09 +0545

    minor changes

--- src/components/organism/Cards/CourseCard/MyCourseCard.tsx
+++ src/components/organism/Cards/CourseCard/MyCourseCard.tsx
@@ -56,12 +56,12 @@ export default function MyCourseCard({ course }: { course: CourseProps }) {
                 <Divider className="my-3!" />
                 <div className="flex flex-col gap-2 md:grid md:grid-cols-2">
                     {course?.course_type !== "free" ? <div className="middle">
-                        {course?.user?.free_trial_expires_at ? <Typography variant="caption" color="error">Expired on</Typography> : <Typography variant="caption">Expires</Typography>}
+                        {!course?.user?.is_free_trial_valid && !course?.user?.has_purchased ? <Typography variant="caption" color="error">Expired on</Typography> : <Typography variant="caption">Expires</Typography>}
                         <div className="flex items-center gap-1.5">
                             <Typography variant="h4">{expiredDay || day}</Typography>
                             <div className="date flex flex-col items-start justify-end">
                                 <Calendar size={12} />
-                                <Typography variant="caption">{expiredMonthAndYear || monthAndYear}</Typography>
+                                <Typography variant="caption">{!course?.user?.is_free_trial_valid ? expiredMonthAndYear : monthAndYear}</Typography>
                             </div>
                         </div>
                     </div> : ""}
