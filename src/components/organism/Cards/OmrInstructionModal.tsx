e1c5c1dde67c4a370f68e0c8c0c34d69e77156fc
arjun <arjunjhukal.makura@gmail.com>
2026-03-23 17:34:21 +0545

    minor changes

--- src/components/organism/Cards/OmrInstructionModal.tsx
+++ src/components/organism/Cards/OmrInstructionModal.tsx
@@ -236,7 +236,6 @@ export default function OmrInstructionModal({
                                     background: (theme) => theme.palette.error.light,
                                 }}
                             >
-
                                 {renderHtml(format.omr_note)}
                             </Box>
                         )}
