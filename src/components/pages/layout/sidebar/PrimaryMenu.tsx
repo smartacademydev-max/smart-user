c413d8fbd5f4ef01b3e9d2060b5d18915763aaff
arjun <arjunjhukal.makura@gmail.com>
2026-04-07 15:26:20 +0545

    chat and discusssion module

--- src/components/pages/layout/sidebar/PrimaryMenu.tsx
+++ src/components/pages/layout/sidebar/PrimaryMenu.tsx
@@ -322,6 +322,53 @@ export default function PrimaryMenu({ isCollapsed = false }: PrimaryMenuProps) {
               </ListItemButton>
             </ListItem>
           )}
+
+          <ListItem disablePadding className="menu__item">
+            <ListItemButton
+              onClick={() => navigate(PATH.DISCUSSION.ROOT)}
+              className={location.pathname.startsWith(PATH.DISCUSSION.ROOT) ? "active" : ""}>
+              <ListItemIcon>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                  <path d="M8 10h8M8 13h5M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12c0 1.6.376 3.112 1.043 4.453L2 22l5.547-1.043A9.955 9.955 0 0012 22z" stroke="#9CA3B0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </ListItemIcon>
+              <ListItemText primary={t("menus.discussion.root")} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding className="menu__item">
+            <ListItemButton
+              onClick={() => navigate(PATH.TICKET.ALL_TICKETS.ROOT)}
+              className={location.pathname.startsWith(PATH.TICKET.ALL_TICKETS.ROOT) ? "active" : ""}>
+              <ListItemIcon>
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M2 9C2 7.89543 2.89543 7 4 7H20C21.1046 7 22 7.89543 22 9V20C22 21.1046 21.1046 22 20 22H4C2.89543 22 2 21.1046 2 20V9Z" stroke="#9CA3B0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M16 7V5C16 3.89543 15.1046 3 14 3H10C8.89543 3 8 3.89543 8 5V7" stroke="#9CA3B0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M12 12V17M9.5 14.5H14.5" stroke="#9CA3B0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </ListItemIcon>
+              <ListItemText primary={t("menus.ticket.root")} />
+              {/* {openTicket ? <ExpandLess /> : <ExpandMore />} */}
+            </ListItemButton>
+            {/* <Collapse in={openTicket} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding sx={{ pl: 3 }}>
+                <ListItem disablePadding className="menu__item">
+                  <ListItemButton
+                    onClick={() => navigate(PATH.TICKET.ALL_TICKETS.ROOT)}
+                    className={location.pathname.startsWith(PATH.TICKET.ALL_TICKETS.ROOT) ? "active-nested" : ""}>
+                    <ListItemText primary={t("menus.ticket.all_tickets")} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding className="menu__item">
+                  <ListItemButton
+                    onClick={() => navigate(PATH.TICKET.CHATS.ROOT)}
+                    className={location.pathname.startsWith(PATH.TICKET.CHATS.ROOT) ? "active-nested" : ""}>
+                    <ListItemText primary={t("menus.ticket.chats")} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Collapse> */}
+          </ListItem>
         </List>
 
         {/* ── OTHERS ── */}
